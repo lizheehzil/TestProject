c16d7a95bbfa991cf2a3fa089226a78a21db81a3
lizhe <442519654@qq.com>
2020-10-03 00:31:13 +0800

    test a moudle -- http-test

---
+++ http-test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	fmt.Println("Please visit http://127.0.0.1:12345")
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		s := fmt.Sprintln("你好世界! -- Time:%s", time.Now().String())
+		fmt.Fprintln(writer, "%v\n", s)
+		log.Printf("%v\n", s)
+
+	})
+	if err := http.ListenAndServe(":12345", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
--- type/main.go
+++ type/main.go
@@ -2,16 +2,45 @@ package main
 
 import "fmt"
 
-func main() {
-	var t1 int
-	var t2 interface{}
-	t3 := new(int)
-	t4 := make([]int, 3, 4)
+type A struct {
+	Anum1 int
+	Anum2 int
+}
 
-	fmt.Printf("t1 type is %T\n", t1)
-	fmt.Printf("t2 type is %T\n", t2)
-	fmt.Printf("t3 type is %T\n", t3)
-	fmt.Printf("t4 type is %T\n", t4)
-	fmt.Print(t4, "\n")
-	fmt.Println(len(t4))
+type B struct {
+	A
+	Bnum1 int
+	Bnum2 int
+}
+
+func main() {
+	//var t1 int
+	//var t2 interface{}
+	//t3 := new(int)
+	//t4 := make([]int, 3, 4)
+	//
+	//fmt.Printf("t1 type is %T\n", t1)
+	//fmt.Printf("t2 type is %T\n", t2)
+	//fmt.Printf("t3 type is %T\n", t3)
+	//fmt.Printf("t4 type is %T\n", t4)
+	//fmt.Print(t4, "\n")
+	//fmt.Println(len(t4))
+	b := new(B)
+	b.Anum1 = 1
+	b.Anum2 = 2
+	b.Bnum1 = 3
+	b.Bnum2 = 4
+
+	c := B{*new(A),
+		1,
+		3}
+
+	//fmt.Printf("type is %T",b)
+	fmt.Println(c.Anum2)
+	fmt.Println(c.Bnum2)
+
+	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", new(B))
+
+
 }
